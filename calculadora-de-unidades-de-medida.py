91878a0c9bfb7adb82b14a3f291199d9dc294a4f
unknown <leticiafcassimiro@gmail.com>
2023-03-23 10:49:38 -0300

    Adição das funções de conversão de quilobyte para megabyte e vice-versa; adição do print das conversões.

--- calculadora-de-unidades-de-medida.py
+++ calculadora-de-unidades-de-medida.py
@@ -26,6 +26,16 @@ def kbyteParaByte(valorASerConvertido):
     bytes2Calculado = valorASerConvertido * CONSTANTE_BASE_CONVERSÃO
     return bytes2Calculado
 
+def kbyteParaMByte(valorASerConvertido):
+    print('Valor convertido de byte para quilobyte:')
+    kbytes2Calculado = valorASerConvertido / CONSTANTE_BASE_CONVERSÃO
+    return kbytes2Calculado
+
+def mbyteParaKbyte(valorASerConvertido):
+    print('Valor convertido de quilobyte para byte:')
+    mbytes1Calculado = valorASerConvertido * CONSTANTE_BASE_CONVERSÃO
+    return mbytes1Calculado
+
 #prints das conversões acima:
 
 print('Insira o valor a ser convertido (byte para bit):')
@@ -47,3 +57,13 @@ print('Insira o valor a ser convertido (quilobyte para byte):')
 entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
 valorConvertido4 = kbyteParaByte(entradaDoTecladoValorASerConvertido)
 print(valorConvertido4)
+
+print('Insira o valor a ser convertido (quilobyte para megabyte):')
+entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+valorConvertido5 = kbyteParaMByte(entradaDoTecladoValorASerConvertido)
+print(valorConvertido5)
+
+print('Insira o valor a ser convertido (megabyte para quilobyte):')
+entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+valorConvertido6 = mbyteParaKbyte(entradaDoTecladoValorASerConvertido)
+print(valorConvertido6)
