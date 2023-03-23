84b2b9c0c8e5b1620fece9106d55ba040bb799eb
unknown <leticiafcassimiro@gmail.com>
2023-03-23 10:23:16 -0300

    Adição das funções de conversão de bit para byte e vice-versa e o print da conversão com um input do usuário.

--- calculadora-de-unidades-de-medida.py
+++ calculadora-de-unidades-de-medida.py
@@ -1,8 +1,21 @@
 CONSTANTE_BASE_CONVERSÃO = 1024
 
 def converterStringParaFloat(value):
-    print('Valor convertido de str para float')
+    print('Valor convertido de string para float')
     return float(value)
 
-print('Insira o valor a ser convertido')
+def bitParaByte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    bytesCalculado = valorASerConvertido / 8
+    return bytesCalculado
+
+def byteParaBit(valorASerConvertido):
+    print('Valor convertido de byte para bit:')
+    bitsCalculado = valorASerConvertido * 8
+    return bitsCalculado
+
+print('Insira o valor a ser convertido:')
 entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+valorConvertido = byteParaBit(entradaDoTecladoValorASerConvertido)
+
+print(valorConvertido)
